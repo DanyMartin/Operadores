8837bf2bcb2e54d624a7ffcfa7117436cc0bda65
CARLOS MARTIN <CARLOS MARTIN@192.168.0.109>
2021-09-14 15:47:45 -0500

    Cambio de codigo

---
+++ src/main/java/com/mycompany/ejercicios13092021/EJERCICIO2.java
@@ -0,0 +1,30 @@
+
+package com.mycompany.ejercicios13092021;
+
+
+import java.util.Scanner;
+
+
+
+
+
+public class EJERCICIO2 {
+
+   
+    public static void main(String[] args) {
+        Scanner entrada = new Scanner(System.in);
+    
+        int horas, semanas, dias, horas1; 
+        horas = entrada.nextInt();
+        System.out.println("Numero de horas que desea: "+ horas);
+        semanas = horas / 168;
+        dias = horas %168 / 24;
+        horas1 = horas%24;
+        System.out.println("El numero de semanas es: "+semanas);
+        System.out.println("El numero de dias es: "+dias);
+        System.out.println("El numero de horas restantes es: "+horas1);
+        
+        
+    }
+    
+}
